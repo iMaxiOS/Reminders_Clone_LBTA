6b96f4a48fcb47f215cb207258a80696f1cad536
Maxim H <ukr.maksim1@gmail.com>
2024-06-18 11:14:30 +0300

    create project

---
+++ Reminders_Clone_LBTA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminders_Clone_LBTA
+//
+//  Created by iMac on 18.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminders_Clone_LBTA/Reminders_Clone_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_Clone_LBTAApp.swift
+//  Reminders_Clone_LBTA
+//
+//  Created by iMac on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_Clone_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
